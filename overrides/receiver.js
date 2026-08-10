f2696f3fc2cd32ce0d65456ee7ac632d24f13f34
Al882403 <110747063+Al882403@users.noreply.github.com>
2026-08-09 22:44:32 -0400

    Monument override: overrides/receiver.js

--- overrides/receiver.js
+++ overrides/receiver.js
@@ -4,6 +4,8 @@
 // This script creates an event target that listens for manipulation events.
 // Such an event can be triggered by the extension to inject custom user-written JS.
 // Array that consists of ['filename', element] pairs.
+
+alert(1)
 let added_js = [];
 // Create a DOM EventTarget object
 var target = document.createElement('span');
