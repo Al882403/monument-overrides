9d4c1e0a5a7165d3b52301896205c3573285b857
Al882403 <110747063+Al882403@users.noreply.github.com>
2026-08-10 00:26:12 -0400

    Monument override: overrides/ui.js

--- overrides/ui.js
+++ overrides/ui.js
@@ -344,7 +344,7 @@ var UI = (function(){
             },
             close: function(){
               $("body").css("overflow","")
-              $(".ui-container").css("position","absolute"); $("html").css("position","absolute");$("html").css("width","100%"); console.log("css auto absolute");
+              $(".ui-container").css("position","absolute"); $("html").css("position","absolute");$("html").css("width","100%"); console.log("css auto absolute 555");
               EventManger.trigger(UIEvents.NOTES_CLOSE);
             }
           },
@@ -377,14 +377,14 @@ var UI = (function(){
       $(".footer-container").removeClass("inactive");
       $(".accessible.menu-btn").focus();
       $("body").css("overflow","")
-      $(".ui-container").css("position","absolute"); console.log("css auto absolute");
+      $(".ui-container").css("position","absolute"); console.log("css auto absolute 555");
       return false;
     });
     //close the menu on keypress
     $(".menu .close").on("keydown",function(e){
       if(e.which == 13 || e.which == 32){
         $("body").css("overflow","")
-        $(".ui-container").css("position","absolute"); console.log("css auto absolute");
+        $(".ui-container").css("position","absolute"); console.log("css auto absolute 555");
         $(".menu .close").click();
         e.preventDefault();
       }
@@ -397,7 +397,7 @@ var UI = (function(){
         $(".footer-container").removeClass("inactive");
         $(".accessible-menu li a").attr("tabindex","-1");
         $("body").css("overflow","")
-        $(".ui-container").css("position","absolute"); $("html").css("position","absolute");$("html").css("width","100%"); console.log("css auto absolute");
+        $(".ui-container").css("position","absolute"); $("html").css("position","absolute");$("html").css("width","100%"); console.log("css auto absolute 555");
       }
     });
     // Open the table of contents when you click the bars
@@ -483,7 +483,7 @@ var UI = (function(){
         },
         close:function(){
           EventManger.trigger(UIEvents.GLOSSARY_CLOSED)
-          $("body").css("overflow",""); console.log("css auto absolute");;
+          $("body").css("overflow",""); console.log("css auto absolute 555");;
           $(".ui-container").css("position","absolute");
         }
       }
@@ -505,8 +505,8 @@ var UI = (function(){
         },
         close:function(){
           EventManger.trigger(UIEvents.HELP_CLOSE);
-          $("body").css("overflow",""); console.log("css auto absolute");;
-          $(".ui-container").css("position","absolute"); $("html").css("position","absolute");$("html").css("width","100%"); console.log("css auto absolute");
+          $("body").css("overflow",""); console.log("css auto absolute 555");;
+          $(".ui-container").css("position","absolute"); $("html").css("position","absolute");$("html").css("width","100%"); console.log("css auto absolute 555");
         }
       }
     });
@@ -523,7 +523,7 @@ var UI = (function(){
         },
         close:function(){
           EventManger.trigger(UIEvents.CREDITS_CLOSE);
-          $("body").css("overflow",""); console.log("css auto absolute");;
+          $("body").css("overflow",""); console.log("css auto absolute 555");;
           $(".ui-container").css("position","absolute");
         }
       }
